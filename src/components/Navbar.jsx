17db34b9e95209bf80597fc9a6d1b5892c3a443c
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-12-03 20:50:05 +0400

    fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,9 +19,6 @@ toast('Вы вышли из системы')
 
   return (
     <div className="flex py-4 justify-between items-center">
-      <span className="flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm ">
-        E
-      </span>
       {isAuth && (
         <ul className="flex gap-8">
           <li>
